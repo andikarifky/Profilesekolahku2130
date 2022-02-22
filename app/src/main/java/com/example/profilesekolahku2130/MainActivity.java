41f0847d3a52f193ad1650944cc0bef8f9a47a77
andikarifky <andikarifky20@gmail.com>
2022-02-22 12:17:17 +0700

    COMMIT

--- app/src/main/java/com/example/profilesekolahku2130/MainActivity.java
+++ app/src/main/java/com/example/profilesekolahku2130/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.profilesekolahku2130;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
